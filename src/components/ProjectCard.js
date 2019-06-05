2135ef28e267f06dec9d3d0a75a51d413b617e99
pmduaree <pmduaree@gmail.com>
2019-06-05 16:59:13 -0700

    Fix responsive work project desktop

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,16 +1,18 @@
-import React  from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import style from "../styles/projectCard.module.css"
 import {Link} from "gatsby"
 
 const ProjectCard = (props) => {
   return (
-    <Link to={props.id} className={style.container}>
-      <img className={style.image} src={props.imageUrl}/>
-      <span className={style.information}>
+    <Link to={props.id}>
+      <div className={style.container}>
+        <img className={style.image} src={props.imageUrl}/>
+        <span className={style.information}>
           <div className={style.name}>{props.name}</div>
           <div className={style.subtitle}>{props.subtitle}</div>
         </span>
+      </div>
     </Link>
   )
 };
