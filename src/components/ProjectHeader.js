d41244f8cae44eb8855f4867a76c195064f78476
pmduaree <pmduaree@gmail.com>
2019-06-05 18:30:05 -0700

    Fix project page in mobile

--- src/components/ProjectHeader.js
+++ src/components/ProjectHeader.js
@@ -1,12 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
+import style from "../styles/ProjectHeader.module.css"
 
 const ProjectHeader = ({title, subtitle}) => (
   <div className={'row'}>
     <div className={'one column'}>&nbsp;</div>
     <div className={'ten columns'}>
-      <h1>{title}</h1>
-      <h4>{subtitle}</h4>
+      <h1 className={style.title}>{title}</h1>
+      <h4 className={style.subtitle}>{subtitle}</h4>
     </div>
   </div>
 );
