a52d0a55b812c5630608fe6d42f666c2a2f05062
pmduaree <pmduaree@gmail.com>
2019-06-09 18:09:42 -0700

    Add mobile menu option

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,68 @@
 import {Link} from "gatsby"
-import React from "react"
+import React, {useState} from "react"
 import logo from '../resources/logo.svg'
 import style from '../styles/header.module.css'
 import PropTypes from 'prop-types';
 
 const Header = ({selectedPage}) => {
   const selectedClass = (pageName) => selectedPage === pageName ? 'link-selected' : '';
+  const [displayMenuModal, setDisplayMenuModal] = useState(false);
+
+  const toggleDisplayMenu = () => {
+    setDisplayMenuModal(!displayMenuModal);
+    document.documentElement.style.overflowY = displayMenuModal ? 'scroll' : 'hidden';
+  };
 
   return (
     <header>
       <div className={[style.header, 'row'].join(' ')}>
-        <Link to="/" className={'four columns'}>
-          <img src={logo}/>
-        </Link>
-        <div className={'two columns'}>&nbsp;</div>
-        <div className={'two columns'}>
-          <Link className={style.anchor} to="work">
-            <span className={[style.text, selectedClass('work')].join(' ')}>
-              Work
-            </span>
+        <div className={[style.headerMobile, 'six columns'].join(' ')}>
+          <Link to="/">
+            <img src={logo}/>
           </Link>
+          <div className={style.headerMenuMobile} onClick={toggleDisplayMenu}>
+            {displayMenuModal ? 'Close' : 'Menu'}
+          </div>
         </div>
-        <div className={'two columns'}>
-          <Link className={style.anchor} to="about-me">
-            <span className={[style.text, selectedClass('about-me')].join(' ')}>
-              About Me
-            </span>
-          </Link>
+        <div className={'six columns'}>
+          <div className={style.headerMenuDesktop}>
+            <Link className={style.anchor} to="work">
+              <span className={[style.text, selectedClass('work')].join(' ')}>
+                Work
+              </span>
+            </Link>
+            <Link className={style.anchor} to="about-me">
+              <span className={[style.text, selectedClass('about-me')].join(' ')}>
+                About Me
+              </span>
+            </Link>
+            <Link className={style.anchor} to="contact">
+              <span className={[style.text, selectedClass('contact')].join(' ')}>
+                Contact
+              </span>
+            </Link>
+          </div>
         </div>
-        <div className={'two columns'}>
-          <Link className={style.anchor} to="contact">
-            <span className={[style.text, selectedClass('contact')].join(' ')}>
-              Contact
-            </span>
-          </Link>
+      </div>
+      <div className={[style.menuMobile, displayMenuModal ? style.show : style.hide].join(' ')}>
+        <div className={"row"}>
+          <div className={"twelve columns"}>
+            <Link className={style.anchor} to="work">
+              <span className={[style.text, selectedClass('work')].join(' ')}>
+                Work
+              </span>
+            </Link>
+            <Link className={style.anchor} to="about-me">
+              <span className={[style.text, selectedClass('about-me')].join(' ')}>
+                About Me
+              </span>
+            </Link>
+            <Link className={style.anchor} to="contact">
+              <span className={[style.text, selectedClass('contact')].join(' ')}>
+                Contact
+              </span>
+            </Link>
+          </div>
         </div>
       </div>
     </header>
